cbea245693bace8a331a4cae16d4a95a6e713240
VitaTurtel <93372861+VitaPeppa@users.noreply.github.com>
2023-04-14 15:19:00 +0100

    e

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const sURL = 'https://vite-api.thomiz.dev/supply/circulating/tti_22d0b205bed4d26
 const swapURL = 'https://vite-api.thomiz.dev/tvl/vitcswap'
 const stakeURL = 'https://vite-api.thomiz.dev/tvl/beefstake'
 const swapvol = 'https://vitcswap-api.thomiz.dev/api/volume/tti_22d0b205bed4d268a05dfc3c'
+const dURL = 'https://discord.com/api/v9/invites/vitamincoin?with_counts=true';
 
 // JSON fetcher
 async function getData(tURL){
@@ -26,13 +27,23 @@ async function holderData() {
 
 // get marketdata
 async function marketData(){
-  let market = await getData(tURL)
+  let market = await getData(tURL);
+  console.log(market)
   let nPrice = market.data[0].closePrice;
   let cPrice = market.data[0].priceChangePercent;
   if(cPrice > 0){pChange = '▲'} else {pChange = '▼'}
   elID('nPrice').innerHTML = nPrice;
   elID('cPrice').innerHTML = pChange + ' '+ (cPrice*100).toFixed(2)+'%';
+  marketData2();
 };
+async function marketData2(){
+  let market2 = await getData(vURL);
+  let volVT = +market2.data.baseVolume;
+  let volVC = +market2.data.volume;
+  elID('volVT24').innerHTML = volVT.toFixed(2)+' VITE';
+  elID('volVC24').innerHTML = volVC.toFixed(2)+' VITC';
+  console.log(market2)
+}
 // get vitcswap and vitcstake TVL & volume
 async function util() {
   let vitcswap = await getData(swapURL)
@@ -41,9 +52,9 @@ async function util() {
   let tvlswap = +vitcswap.tvl
   let tvlstake = +vitcstake.tvl
   let volswap = +vol.volume
-  elID('tvlswap').innerHTML =  "$" + tvlswap.toFixed(2)
-  elID('tvlstake').innerHTML = "$" + tvlstake.toFixed(2)
-  elID('volswap').innerHTML =  "$" + volswap.toFixed(2)
+  elID('tvlswap').innerHTML =   '$'+tvlswap.toFixed(2);
+  elID('tvlstake').innerHTML =  '$'+tvlstake.toFixed(2);
+  elID('volswap').innerHTML =   '$'+volswap.toFixed(2);
 }
 // get supply and burn data
 async function supplyData(){
@@ -56,14 +67,24 @@ async function supplyData(){
   elID('circS').innerHTML = cirP.toFixed(2)+'%';
   elID('circD').innerHTML = (cirS/1000000).toFixed(2)+'M'+' / '+(maxS/1000000).toFixed(2)+'M'
   elID('burnS').innerHTML = brnP.toFixed(2)+'%';
-  elID('burnD').innerHTML = (brnS/1000000).toFixed(2)+'M'+' / 1B'
+  elID('burnD').innerHTML = (brnS/1000000).toFixed(2)+'M'+' / '+(maxS/1000000).toFixed(2)+'M'
 }
+
+// get discord data
+async function dcData(){
+  let dcraw = await getData(dURL);
+  console.log(await dcraw);
+  elID('discordMembers').innerHTML = dcraw.approximate_member_count;
+}
+
 // Loader
 async function loader(){
+  await dcData();
   await holderData();
   await marketData();
   await supplyData();
-  await util()
+  await util();
+
 };
 loader();
 // DOM Script
